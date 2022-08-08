460d9193c83e0bac9d2b08f6be0783d85dd52a32
midbel <brun.nicolas21@gmail.com>
2022-08-08 16:57:59 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/midbel/slices
+
+go 1.18
---
+++ slices.go
@@ -0,0 +1,25 @@
+package slices
+
+func At[T any](args []T, ix int) T {
+	var x T
+	if ix < 0 || ix >= len(args) {
+		return x
+	}
+	return args[ix]
+}
+
+func Fst[T any](args []T) T {
+	return args[0]
+}
+
+func Snd[T any](args []T) T {
+	return args[1]
+}
+
+func Lst[T any](args []T) T {
+	return args[len(args)-1]
+}
+
+func Rest[T any](args []T) []T {
+	return args[1:]
+}
