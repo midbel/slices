5b649f020f2e92fd6db2474f2192de9ae3d73f44
midbel <brun.nicolas21@gmail.com>
2022-08-09 10:08:50 +0200

    return default if length does not match + Filter

--- slices.go
+++ slices.go
@@ -9,18 +9,33 @@ func At[T any](args []T, ix int) T {
 }
 
 func Fst[T any](args []T) T {
+	if len(args) == 0 {
+		var x T
+		return x
+	}
 	return args[0]
 }
 
 func Snd[T any](args []T) T {
+	if len(args) < 2 {
+		var x T
+		return x
+	}
 	return args[1]
 }
 
 func Lst[T any](args []T) T {
+	if len(args) == 0 {
+		var x T
+		return x
+	}
 	return args[len(args)-1]
 }
 
 func Rest[T any](args []T) []T {
+	if len(args) == 0 {
+		return nil
+	}
 	return args[1:]
 }
 
@@ -29,4 +44,15 @@ func Take[T any](args []T, n int) []T {
 		return nil
 	}
 	return args[n:]
+}
+
+func Filter[T any](args []T, fn func(T) bool) []T {
+	list := make([]T, 0, len(args))
+	for i := range args {
+		if !fn(args[i]) {
+			continue
+		}
+		list = append(list, args[i])
+	}
+	return list
 }
