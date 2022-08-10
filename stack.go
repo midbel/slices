0ecf1650163bb7d9cd79e0ea89e5cbfab736034e
midbel <brun.nicolas21@gmail.com>
2022-08-10 13:01:21 +0200

    Stack type

---
+++ stack.go
@@ -0,0 +1,28 @@
+package slices
+
+type Stack[T any] struct {
+  list []T
+}
+
+func New[T any]() *Stack[T] {
+  return &Stack[T]{}
+}
+
+func (s *Stack[T]) Push(t T) {
+  s.list = append(s.list, t)
+}
+
+func (s *Stack[T]) Pop() T {
+  var (
+    t = Lst(s.list)
+    n = len(s.list)
+  )
+  if n > 0 {
+    s.list = s.list[:n-1]
+  }
+  return t
+}
+
+func (s *Stack) Len() int {
+  return len(s.list)
+}
