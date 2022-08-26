62e6c38312faf1b3556edd8343001007e0649f81
midbel <brun.nicolas21@gmail.com>
2022-08-26 19:54:47 +0200

    gofmt

--- stack.go
+++ stack.go
@@ -1,28 +1,28 @@
 package slices
 
 type Stack[T any] struct {
-  list []T
+	list []T
 }
 
 func New[T any]() *Stack[T] {
-  return &Stack[T]{}
+	return &Stack[T]{}
 }
 
 func (s *Stack[T]) Push(t T) {
-  s.list = append(s.list, t)
+	s.list = append(s.list, t)
 }
 
 func (s *Stack[T]) Pop() T {
-  var (
-    t = Lst(s.list)
-    n = len(s.list)
-  )
-  if n > 0 {
-    s.list = s.list[:n-1]
-  }
-  return t
+	var (
+		t = Lst(s.list)
+		n = len(s.list)
+	)
+	if n > 0 {
+		s.list = s.list[:n-1]
+	}
+	return t
 }
 
 func (s *Stack[T]) Len() int {
-  return len(s.list)
+	return len(s.list)
 }
