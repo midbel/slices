341b7b6b9b7a6f452068db3e9c13df984b439296
midbel <brun.nicolas21@gmail.com>
2022-09-17 16:58:04 +0200

    Shuffle/Foreach/Apply/Map functionns

--- slices.go
+++ slices.go
@@ -1,5 +1,9 @@
 package slices
 
+import (
+	"math/rand"
+)
+
 func At[T any](args []T, ix int) T {
 	var x T
 	if ix < 0 || ix >= len(args) {
@@ -89,6 +93,32 @@ func Reverse[T any](args []T) []T {
 		j--
 	}
 	return list
+}
+
+func Shuffle[T any](args []T) []T {
+	rand.Shuffle(len(args), func(i, j int) {
+		args[i], args[j] = args[j], args[i]
+	})
+	return args
+}
+
+func Foreach[T any](args []T, do func(T)) {
+	for i := range args {
+		do(args[i])
+	}
+}
+
+func Apply[T any](args []T, do func(T) T) {
+	for i := range args {
+		args[i] = do(args[i])
+	}
+}
+
+func Map[T any](args []T, do func(T) T) []T {
+	vs := make([]T, len(args))
+	copy(vs, args)
+	Apply(vs, do)
+	return vs
 }
 
 func Prepend[T any](fst T, rest []T) []T {
