13bff9aa89daa11b239881e0035d012021df47fc
midbel <brun.nicolas21@gmail.com>
2022-11-25 10:00:37 +0100

    Ring type to implement a ring buffer

---
+++ ring.go
@@ -0,0 +1,34 @@
+package slices
+
+type Ring[T any] struct {
+	list []T
+	size int64
+	curr int64
+}
+
+func NewRing[T any](list []T) *Ring[T] {
+	var (
+		tmp = make([]T, len(list))
+		size = copy(tmp, list)
+	)
+	return &Ring[T]{
+		list: tmp,
+		size: int64(size),
+	}
+}
+
+func (r *Ring[T]) Next() T {
+	if len(r.list) == 0 {
+		var zero T
+		return zero
+	}
+	defer r.next()
+	return r.list[r.curr%r.size]
+}
+
+func (r *Ring[T]) next() {
+	r.curr++
+	if r.curr < 0 {
+		r.curr = 0
+	}
+}
